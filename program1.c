dc28f4749d4148500bedaa6c3af697387092a2f9
Radhika2611 <66779245+Radhika2611@users.noreply.github.com>
2022-10-11 22:48:36 +0530

    Update program1.c

--- program1.c
+++ program1.c
@@ -1,1 +1,10 @@
-
+#inlcude<stdio.h>
+void main()
+{
+  int i,n,sum=0;
+  printf("enter the number of elements to be added");
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+    sum=sum+i;
+  printf("Sum of n elements =%d",sum);
+}
